6ba0b8eb03d188c5e5fadc29630fa273a1d1aa3b
Zein <zeinkasar123@gmail.com>
2023-09-28 14:28:32 +0200

    dist

--- vue.config.js
+++ vue.config.js
@@ -2,15 +2,3 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
-
-module.exports = {
-  devServer: {
-    proxy: {
-      '^/users': {
-        target: 'http://localhost:5173/',
-        ws: true,
-        changeOrigin: true
-      },
-    }
-  }
-} 
